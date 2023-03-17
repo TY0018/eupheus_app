b1157eea83a14c129ef9ee4b0cd5f5959cfb3949
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-03-17 22:44:43 +0800

    created app structure, bottom navigation bar

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:eupheus_app/explore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // theme of the app: const js means this thing will nvr change
+      debugShowCheckedModeBanner: false, // remove debug banner
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  // stateful means page can be reloaded
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  int currentPage = 0; //created a vairable, put before build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Eupheus'),
+          ),
+        body: const Explore(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.home),
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.explore), label: 'Explore'), //explore page
+            NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
+            NavigationDestination(icon: Icon(Icons.groups), label: 'Community'),
+            NavigationDestination(icon: Icon(Icons.sports_esports), label: 'Games'),
+            NavigationDestination(icon: Icon(Icons.face), label: 'Me')
+          ],
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPage = index;
+            });
+          },
+          selectedIndex: currentPage,
+        ));
+  }
+}
