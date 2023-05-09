d149b487aa449bcf1f2d322f549a0b1747cbffab
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-05-09 15:43:06 +0800

    tiktok ui

--- lib/explore.dart
+++ lib/explore.dart
@@ -6,41 +6,105 @@ class Explore extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100.0,
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        ToggleButton(),
-        Icon(Icons.star, size: 35.0, color: '#8C84EE'.toColor()),
-        Icon(Icons.share_outlined, size: 35.0, color: '#8C84EE'.toColor()),
-      ]),
+    return Scaffold(
+      body: Stack(
+        children: [
+          // user post (at the very back)
+          // Container(
+          //   color: Colors.deepPurple[300],
+          // ),
+
+          // user name and caption
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment(-1, 1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("Sustainability",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "Introduction to Cultural Sustainability",
+                            style: TextStyle(color: Colors.black)),
+                        TextSpan(
+                            text: ' #Sustainability #CulturePreservation',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.black)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // buttons
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment(1, 1),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.favorite, size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 10),
+                  Text("30k"),
+                  SizedBox(height: 20),
+                  Icon(Icons.star, size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 10),
+                  Text("25k"),
+                  SizedBox(height: 20),
+                  Icon(Icons.share_outlined,
+                      size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 20),
+                  Text("10k"),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
-class ToggleButton extends StatefulWidget {
-  const ToggleButton(
-    {super.key});
+// class ToggleButton extends StatefulWidget {
+//   const ToggleButton({super.key});
 
-  @override
-  State<ToggleButton> createState() => _ToggleButtonState();
-}
+//   @override
+//   State<ToggleButton> createState() => _ToggleButtonState();
+// }
 
-class _ToggleButtonState extends State<ToggleButton> {
-  bool selected = false;
+// class _ToggleButtonState extends State<ToggleButton> {
+//   bool selected = false;
 
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      isSelected: selected,
-      icon: Icon(Icons.favorite_border_outlined),
-      selectedIcon: Icon(Icons.favorite),
-      onPressed: () {
-        setState(() {
-          selected = !selected;
-        });
-      },
-      iconSize: 35.0,
-      color:'#8C84EE'.toColor(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return IconButton(
+//       isSelected: selected,
+//       icon: Icon(Icons.favorite_border_outlined),
+//       selectedIcon: Icon(Icons.favorite),
+//       onPressed: () {
+//         setState(() {
+//           selected = !selected;
+//         });
+//       },
+//       iconSize: 35.0,
+//       color: '#8C84EE'.toColor(),
+//     );
+//   }
+// }
+
