7e10a93fee45ec31713d7a108b6313660088f5d5
TY0018 <111295767+TY0018@users.noreply.github.com>
2023-05-11 16:09:39 +0800

    pageview explore

--- lib/explore.dart
+++ lib/explore.dart
@@ -1,6 +1,7 @@
 import 'package:eupheus_app/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:eupheus_app/explore_video.dart';
+import 'package:eupheus_app/sustainability_video.dart';
 
 class Explore extends StatelessWidget {
   const Explore({super.key});
@@ -8,7 +9,118 @@ class Explore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Stack(children: [AiPages(), 
+      
+      Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment(1, 1),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Icon(Icons.favorite, size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 5),
+                  Text("30k"),
+                  SizedBox(height: 10),
+                  Icon(Icons.star, size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 5),
+                  Text("25k"),
+                  SizedBox(height: 10),
+                  Icon(Icons.share_outlined,
+                      size: 25.0, color: '#8C84EE'.toColor()),
+                  SizedBox(height: 5),
+                  Text("10k"),
+                ],
+              ),
+            ),
+          )]));}}
+
+
+class AiPages extends StatefulWidget {
+  const AiPages({super.key});
+
+  @override
+  State<AiPages> createState() => _AiPagesState();
+}
+
+class _AiPagesState extends State<AiPages> {
+
+  final _controller = PageController(
+    initialPage: 0,
+  );
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      scrollDirection: Axis.vertical,
+      children: const [
+        SustainabilityPage(),
+        CulturePage(),
+      ],
+    );
+  }
+}
+class SustainabilityPage extends StatelessWidget {
+  const SustainabilityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Stack(
+        children: [EarthVideo(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              alignment: Alignment(-1, 1),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text("How To: Environmental Sustainability",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16,
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "Topic: Sustainability",
+                            style: TextStyle(color: Colors.black)),
+                            
+                        TextSpan(
+                            text: '\n#Sustainability #SavemyWorld, #Earth',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.black)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )],
+    ));
+  }
+}
+
+class CulturePage extends StatelessWidget {
+  const CulturePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Stack(
         children: [VideoCS(),
           Padding(
             padding: const EdgeInsets.all(20.0),
@@ -45,62 +157,7 @@ class Explore extends StatelessWidget {
                 ],
               ),
             ),
-          ),
-
-          // buttons
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Container(
-              alignment: Alignment(1, 1),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Icon(Icons.favorite, size: 25.0, color: '#8C84EE'.toColor()),
-                  SizedBox(height: 5),
-                  Text("30k"),
-                  SizedBox(height: 10),
-                  Icon(Icons.star, size: 25.0, color: '#8C84EE'.toColor()),
-                  SizedBox(height: 5),
-                  Text("25k"),
-                  SizedBox(height: 10),
-                  Icon(Icons.share_outlined,
-                      size: 25.0, color: '#8C84EE'.toColor()),
-                  SizedBox(height: 5),
-                  Text("10k"),
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+          )],
+    ));
   }
 }
-
-// class ToggleButton extends StatefulWidget {
-//   const ToggleButton({super.key});
-
-//   @override
-//   State<ToggleButton> createState() => _ToggleButtonState();
-// }
-
-// class _ToggleButtonState extends State<ToggleButton> {
-//   bool selected = false;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return IconButton(
-//       isSelected: selected,
-//       icon: Icon(Icons.favorite_border_outlined),
-//       selectedIcon: Icon(Icons.favorite),
-//       onPressed: () {
-//         setState(() {
-//           selected = !selected;
-//         });
-//       },
-//       iconSize: 35.0,
-//       color: '#8C84EE'.toColor(),
-//     );
-//   }
-// }
-
---
+++ lib/sustainability_video.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+import 'dart:core';
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:eupheus_app/extensions.dart';
+
+class EarthVideo extends StatelessWidget {
+  const EarthVideo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return EarthVideoScreen();
+  }
+}
+
+class EarthVideoScreen extends StatefulWidget {
+  const EarthVideoScreen({super.key});
+
+  @override
+  State<EarthVideoScreen> createState() => EarthVideoScreenState();
+}
+
+class EarthVideoScreenState extends State<EarthVideoScreen> {
+  late VideoPlayerController _controller;
+  // late Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller =
+        VideoPlayerController.asset('aivideos/Sustainability.mp4');
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.initialize().then((_) => setState(() {}));
+    _controller.play();
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Complete the code in the next step.
+    return Scaffold(
+        body: Container(
+      alignment: Alignment.center,
+      child: AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller)),
+    )
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     // Wrap the play or pause in a call to `setState`. This ensures the
+        //     // correct icon is shown.
+        //     setState(() {
+        //       // If the video is playing, pause it.
+        //       if (_controller.value.isPlaying) {
+        //         _controller.pause();
+        //       } else {
+        //         // If the video is paused, play it.
+        //         _controller.play();
+        //       }
+        //     });
+        //   },
+        //   // Display the correct icon depending on the state of the player.
+        //   backgroundColor: '#FFA183'.toColor(),
+        //   child: Icon(
+        //     _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        //   ),
+        // ),
+        );
+  }
+}
